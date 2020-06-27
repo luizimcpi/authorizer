a32be177d99c3ef73abc9e1b2e227cb6a63c1ce8
Luiz Evangelista <luizhenrique.se@gmail.com>
2020-06-27 17:13:16 -0300

    feat: add account repository test improving performance of tests

--- src/test/kotlin/com/nu/authorizer/domain/services/AccountServiceTests.kt
+++ src/test/kotlin/com/nu/authorizer/domain/services/AccountServiceTests.kt
@@ -5,15 +5,23 @@ import com.nu.authorizer.domain.model.requests.AccountRequest
 import com.nu.authorizer.resources.repositories.AccountInMemoryRepository
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 
 class AccountServiceTests {
 
+    private lateinit var accountService: AccountService
+    private lateinit var accountRepository: AccountInMemoryRepository
+
+    @BeforeEach
+    fun setUp(){
+        accountRepository = AccountInMemoryRepository()
+        accountService = AccountService(accountRepository)
+    }
+
     @Test
-    fun `when receive valid request should save with success`() {
-        val accountRepository = AccountInMemoryRepository()
-        val accountService = AccountService(accountRepository)
+    fun `when receive valid accountRequest should not throw any exception`() {
         val validAccount = Account(activeCard = true, availableLimit = 100)
         val validRequest = AccountRequest(validAccount)
         val e = Exception()
@@ -22,9 +30,17 @@ class AccountServiceTests {
     }
 
     @Test
-    fun `when receive an account that already registered should return response with violations`() {
-        val accountRepository = AccountInMemoryRepository()
-        val accountService = AccountService(accountRepository)
+    fun `when receive valid  accountRequest should create with success`() {
+        val validAccount = Account(activeCard = true, availableLimit = 100)
+        val validRequest = AccountRequest(validAccount)
+        val validResponse = accountService.create(validRequest)
+        assertTrue(validResponse.violations.isEmpty())
+        assertEquals(true, validResponse.account.activeCard)
+        assertEquals(100, validResponse.account.availableLimit)
+    }
+
+    @Test
+    fun `when receive an accountRequest with account that already registered should return response with violations`() {
         val validAccount = Account(activeCard = true, availableLimit = 100)
         val validRequest = AccountRequest(validAccount)
         val repeatedAccount = Account(activeCard = true, availableLimit = 300)
@@ -36,6 +52,7 @@ class AccountServiceTests {
 
         assertTrue(validResponse.violations.isEmpty())
         assertEquals(expectedViolationArray.first(), violationResponse.violations.first())
-        assertEquals(100, violationResponse.account.availableLimit)
+        assertEquals(validResponse.account.activeCard, violationResponse.account.activeCard)
+        assertEquals(validResponse.account.availableLimit, violationResponse.account.availableLimit)
     }
 }
---
+++ src/test/kotlin/com/nu/authorizer/resources/respositories/AccountInMemoryRepositoryTests.kt
@@ -0,0 +1,31 @@
+package com.nu.authorizer.resources.respositories
+
+import com.nu.authorizer.domain.model.entities.Account
+import com.nu.authorizer.resources.repositories.AccountInMemoryRepository
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.assertNull
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class AccountInMemoryRepositoryTests {
+
+    private lateinit var accountRepository: AccountInMemoryRepository
+
+    @BeforeEach
+    fun setUp(){
+        accountRepository = AccountInMemoryRepository()
+    }
+
+    @Test
+    fun `when receive first time account should return null`() {
+        val validAccount = Account(activeCard = true, availableLimit = 100)
+        assertNull(accountRepository.getAccount(validAccount))
+    }
+
+    @Test
+    fun `when receive valid account should create with success`() {
+        val validAccount = Account(activeCard = true, availableLimit = 100)
+        accountRepository.create(validAccount)
+        assertNotNull(accountRepository.getAccount(validAccount))
+    }
+}
