90f6a33de85f11735308edb42f63cfdff3f24749
Luiz Evangelista <luizhenrique.se@gmail.com>
2020-07-01 01:24:59 -0300

    feat: finish implementation of similar requests in small interval

--- src/main/kotlin/com/nu/authorizer/domain/services/TransactionService.kt
+++ src/main/kotlin/com/nu/authorizer/domain/services/TransactionService.kt
@@ -1,5 +1,6 @@
 package com.nu.authorizer.domain.services
 
+import com.nu.authorizer.domain.common.constants.Constants.DOUBLED_TRANSACTION
 import com.nu.authorizer.domain.common.constants.Constants.HIGH_FREQUENCY_SMALL_INTERVAL
 import com.nu.authorizer.domain.common.constants.Constants.VIOLATION_CARD_NOT_ACTIVE
 import com.nu.authorizer.domain.common.constants.Constants.VIOLATION_INSUFICIENT_LIMIT
@@ -48,22 +49,33 @@ class TransactionService(
             violations.add(HIGH_FREQUENCY_SMALL_INTERVAL)
         }
         if (existsSimilarTransactionsOnTwoMinuteInterval()) {
-            violations.add(HIGH_FREQUENCY_SMALL_INTERVAL)
+            violations.add(DOUBLED_TRANSACTION)
         }
     }
 
     private fun existsSimilarTransactionsOnTwoMinuteInterval(): Boolean {
+        val lastTransactions = repository.getLastTransactions().take(3)
+        val sameAmount = lastTransactions.map { it.amount }.toList().stream().distinct().count() <= 1
+        val sameMerchant = lastTransactions.map { it.merchant }.toList().stream().distinct().count() <= 1
+
+        if (sameAmount && sameMerchant) {
+            return existsTransactionsOnTwoMinuteInterval(true)
+        }
         return false
     }
 
-    private fun existsTransactionsOnTwoMinuteInterval(): Boolean {
-        val lastTransactions = repository.getLastTransactions()
-        val allTransactionsInTheSameDay = lastTransactions.size == 4 && lastTransactions
-            .map { it.time.toLocalDate() }
-            .toList()
-            .stream()
-            .distinct()
-            .count() <= 1
+    private fun existsTransactionsOnTwoMinuteInterval(similarTransactions: Boolean = false): Boolean {
+        val lastTransactions = if (similarTransactions) {
+            repository.getLastTransactions().take(3)
+        } else {
+            repository.getLastTransactions()
+        }
+        val allTransactionsInTheSameDay =
+            if (similarTransactions) lastTransactions.size == 3
+            else lastTransactions.size == 4 && lastTransactions.map { it.time.toLocalDate() }.toList()
+                .stream()
+                .distinct()
+                .count() <= 1
 
         val allTransactionsInTheSameHour = lastTransactions.map { it.time.hour }
             .toList()
@@ -76,6 +88,7 @@ class TransactionService(
             val firstDateTime = lastTransactions.map { it.time }.toList().last()
             if (getDifferenceInMinutes(firstDateTime, lastDateTime) <= 2) return true
         }
+
         return false
     }
 }
